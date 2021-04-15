9e89a2a6b995ccb0da359482d07d8b969dddf4b4
vinod143341 <vk1893307@gmail.com>
2021-04-15 13:43:02 +0530

    dot net

---
+++ FlowerShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FlowerShop.Startup))]
+namespace FlowerShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
